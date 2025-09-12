ccd8d9b19317f45b021716909c60cfb0d627bbe9
Brendan Beck <brendan.beck62@gmail.com>
2025-09-12 16:59:54 -0700

    add vim colors to install list

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ DOTS=~/dots
 
 ln -sf $DOTS/.bashrc ~/.bashrc
 ln -sf $DOTS/.vimrc ~/.vimrc
+mkdir -p ~/.vim/colors
+ln -sf $DOTS/.vim/colors/gruvbox.vim ~/.vim/colors/gruvbox.vim
 ln -sf $DOTS/.tmux.conf ~/.tmux.conf
 
 bash $DOTS/gitconfigure.sh
